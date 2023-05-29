d2f78f06085e35aa26ec2446b97a8d757a6bb714
Yassin-ck <mohammedyassinck@gmail.com>
2023-05-29 19:31:29 +0530

    update

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-
+from .models import Departments,Doctors
+from .forms import BookingForm
 
 def index(request):
     return render(request,'index.html')
@@ -12,11 +12,26 @@ def contact(request):
     return render(request,'contact.html')
 
 def department(request):
-    return render(request,'department.html')
+    dict_dept = {
+        'dept':Departments.objects.all()
+    }
+    return render(request,'department.html',dict_dept)
 
 def doctor(request):
-    return render(request,'doctors.html' )
+    dict_docs = {
+        'doctors':Doctors.objects.all()
+    }
+    return render(request,'doctors.html',dict_docs )
 
 def booking(request):
-    return render(request,'booking.html')
+    if request.method == 'POST' :
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()  
+            return render(request,'confirmation.html')
+    form = BookingForm()
+    dict_form ={
+        'form':form
+    }
+    return render(request,'booking.html',dict_form)
 # Create your views here.
--- hospitality/settings.py
+++ hospitality/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+   
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,10 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/media/'
+
+
